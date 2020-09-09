cf7d1f34209491a2e2e98191e1ec79a5d42ce2e8
LordMZTE <lord@mzte.de>
2020-09-09 16:38:08 +0200

    use yaml file for args instead of builder

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,30 @@
+#[macro_use]
+extern crate clap;
+
 use std::error::Error;
 use std::io::Cursor;
 
 use asciify::AsciiBuilder;
 use async_minecraft_ping::ConnectionConfig;
-use clap::{App, Arg};
+use clap::App;
 use image::ImageFormat;
 use itertools::Itertools;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let matches = App::new("mcstat")
-        .about("queries information about a minecraft server")
-        .arg(
-            Arg::with_name("ip")
-                .value_name("IP_ADDRESS")
-                .help("the ip of the server to ping")
-                .takes_value(true)
-                .index(1)
-                .required(true)
-        )
-        .arg(
-            Arg::with_name("port")
-                .value_name("PORT")
-                .help("the port of the server")
-                .long("port")
-                .short("p")
-                .default_value("25565")
-                .takes_value(true)
-        )
-        .arg(
-            Arg::with_name("protocol-version")
-                .long("protocol")
-                .value_name("PROTOCOL_VERSION")
-                .help("the protocol version to use")
-                .default_value("751")
-                .takes_value(true)
-        )
-        .arg(
-            Arg::with_name("image")
-                .short("i")
-                .help("if an the server\'s favicon should be printed as ASCII art")
-        )
-        .arg(
-            Arg::with_name("color")
-                .short("c")
-                .help("if the favicon image should be printed with ANSII color formatting or monochrome")
-        )
-        .arg(
-            Arg::with_name("size")
-                .short("s")
-                .help("the size of the image")
-                .takes_value(true)
-                .default_value("16")
-        )
-        .get_matches();
+    let yaml = load_yaml!("args.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 
+
+    //region Network
     let mut config = ConnectionConfig::build(matches.value_of("ip").unwrap().to_owned());
     config = config.with_port(matches.value_of("port").unwrap().parse().ok().and_then(|p| if p > 0 && p < u16::MAX { Some(p) } else { None }).expect("invalid port"));
     config = config.with_protocol_version(matches.value_of("protocol-version").unwrap().parse().expect("invalid protocol version"));
     let mut connection = config.connect().await?;
     let response = connection.status().await?;
+    //endregion
 
-
+    //region printing
     macro_rules! print_table {
         (s $l:expr => $k:expr) => {
             println!("{: <20} | {}", $l, $k);
@@ -88,7 +51,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         };
     }
 
-    //region printing
     let player_sample = response.players.sample.unwrap_or_default().iter().map(|p| p.name.as_str()).intersperse(", ").collect::<String>();
     print_table!(
         me "Description" => remove_formatting(&response.description.text),
